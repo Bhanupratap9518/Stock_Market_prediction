def4d3c996fdded79e1228893433b40506cbd6fe
Bhanupratap Mohite <153425822+Bhanupratap9518@users.noreply.github.com>
2024-09-26 10:50:39 +0530

    Update stock_prediction_code.py

--- stock_prediction_code.py
+++ stock_prediction_code.py
@@ -1,85 +1,96 @@
+# Import necessary libraries
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+# Set plot styles
+sns.set(style='whitegrid')
 %matplotlib inline
 
-from matplotlib.pylab import rcParams
-rcParams['figure.figsize']=20,10
+# Load the dataset
+df = pd.read_csv("NSE-TATA.csv")
+df["Date"] = pd.to_datetime(df.Date, format="%Y-%m-%d")
+df.set_index("Date", inplace=True)
 
-from sklearn.preprocessing import MinMaxScaler
-scaler=MinMaxScaler(feature_range=(0,1))
+# Plot the closing price history
+plt.figure(figsize=(16, 8))
+plt.plot(df["Close"], label='Close Price History')
+plt.title('Tata Stock Price History')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
 
-df=pd.read_csv("NSE-TATA.csv")
-df.head()
+# Data preprocessing
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(df[['Close']].values)
 
-df["Date"]=pd.to_datetime(df.Date,format="%Y-%m-%d")
-df.index=df['Date']
+# Create training and validation datasets
+train_data = scaled_data[:int(len(scaled_data) * 0.8)]
+valid_data = scaled_data[int(len(scaled_data) * 0.8):]
 
-plt.figure(figsize=(16,8))
-plt.plot(df["Close"],label='Close Price history')
+# Prepare the training data
+x_train, y_train = [], []
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+x_train = np.array(x_train)
+y_train = np.array(y_train)
 
-from keras.models import Sequential
-from keras.layers import LSTM,Dropout,Dense
+# Reshape data for LSTM [samples, time steps, features]
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-data=df.sort_index(ascending=True,axis=0)
-new_dataset=pd.DataFrame(index=range(0,len(df)),columns=['Date','Close'])
+# Build the LSTM model
+lstm_model = Sequential()
+lstm_model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+lstm_model.add(Dropout(0.2))
+lstm_model.add(LSTM(units=50, return_sequences=False))
+lstm_model.add(Dropout(0.2))
+lstm_model.add(Dense(units=1))  # Output layer
 
-for i in range(0,len(data)):
-    new_dataset["Date"][i]=data['Date'][i]
-    new_dataset["Close"][i]=data["Close"][i]
-    
+# Compile the model
+lstm_model.compile(optimizer='adam', loss='mean_squared_error')
 
-new_dataset.index=new_dataset.Date
-new_dataset.drop("Date",axis=1,inplace=True)
+# Set callbacks for early stopping and model checkpointing
+early_stopping = EarlyStopping(monitor='loss', patience=5)
+model_checkpoint = ModelCheckpoint("best_lstm_model.h5", save_best_only=True)
 
-final_dataset=new_dataset.values
+# Train the model
+lstm_model.fit(x_train, y_train, epochs=50, batch_size=32, callbacks=[early_stopping, model_checkpoint])
 
-train_data=final_dataset[0:987,:]
-valid_data=final_dataset[987:,:]
+# Prepare the validation data
+inputs_data = scaled_data[len(scaled_data) - len(valid_data) - 60:]
+inputs_data = inputs_data.reshape(-1, 1)
+inputs_data = scaler.transform(inputs_data)
 
-scaler=MinMaxScaler(feature_range=(0,1))
-scaled_data=scaler.fit_transform(final_dataset)
+# Create test data
+X_test = []
+for i in range(60, inputs_data.shape[0]):
+    X_test.append(inputs_data[i-60:i, 0])
+X_test = np.array(X_test)
 
-x_train_data,y_train_data=[],[]
+# Reshape test data for LSTM
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-for i in range(60,len(train_data)):
-    x_train_data.append(scaled_data[i-60:i,0])
-    y_train_data.append(scaled_data[i,0])
-    
-x_train_data,y_train_data=np.array(x_train_data),np.array(y_train_data)
+# Make predictions
+predictions = lstm_model.predict(X_test)
+predictions = scaler.inverse_transform(predictions)
 
-x_train_data=np.reshape(x_train_data,(x_train_data.shape[0],x_train_data.shape[1],1))
+# Visualize the results
+train_data = df[:int(len(df) * 0.8)]
+valid_data = df[int(len(df) * 0.8):]
+valid_data['Predictions'] = predictions
 
-lstm_model=Sequential()
-lstm_model.add(LSTM(units=50,return_sequences=True,input_shape=(x_train_data.shape[1],1)))
-lstm_model.add(LSTM(units=50))
-lstm_model.add(Dense(1))
-
-
-
-
-lstm_model.compile(loss='mean_squared_error',optimizer='adam')
-lstm_model.fit(x_train_data,y_train_data,epochs=1,batch_size=1,verbose=2)
-
-inputs_data=new_dataset[len(new_dataset)-len(valid_data)-60:].values
-inputs_data=inputs_data.reshape(-1,1)
-inputs_data=scaler.transform(inputs_data)
-
-
-X_test=[]
-for i in range(60,inputs_data.shape[0]):
-    X_test.append(inputs_data[i-60:i,0])
-X_test=np.array(X_test)
-
-X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
-closing_price=model.predict(X_test)
-closing_price=scaler.inverse_transform(closing_price)
-
-lstm_model.save("saved_lstm_model.h5")
-
-train_data=new_dataset[:987]
-valid_data=new_dataset[987:]
-valid_data['Predictions']=prediction_closing
-plt.plot(train_data["Close"])
-plt.plot(valid_data[['Close',"Predictions"]])
+plt.figure(figsize=(16, 8))
+plt.plot(train_data['Close'], label='Training Data')
+plt.plot(valid_data[['Close', 'Predictions']], label='Validation Data and Predictions')
+plt.title('Tata Stock Price Prediction')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
